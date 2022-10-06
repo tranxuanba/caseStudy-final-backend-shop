73f1114a000b62f87237020811a8bd955040aa88
ishopjapan <thoitran2107@gmail.com>
2022-10-06 15:00:15 +0700

    tao du an

---
+++ src/main/java/com/casestudy/CasestudyFinalBackendShopApplication.java
@@ -0,0 +1,13 @@
+package com.casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasestudyFinalBackendShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasestudyFinalBackendShopApplication.class, args);
+    }
+
+}

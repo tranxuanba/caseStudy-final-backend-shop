83f1a9b790337ea6cc8641cd308040f0852a5f53
ishopjapan <thoitran2107@gmail.com>
2022-10-06 15:19:17 +0700

    update

---
+++ src/main/java/com/casestudy/model/AppUser.java
@@ -0,0 +1,20 @@
+package com.casestudy.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+
+}

8db2f0e87c858857e9fb2809750f1beeff89bc58
ishopjapan <thoitran2107@gmail.com>
2022-10-07 11:05:45 +0700

    lam view, cate

--- src/main/java/com/casestudy/controllers/LoginAPI.java
+++ src/main/java/com/casestudy/controllers/LoginAPI.java
@@ -1,17 +1,17 @@
-//package com.casestudy.controllers;
-//
-//import com.casestudy.model.LoginUser;
-//import com.casestudy.model.UserRole;
-//import com.casestudy.model.dto.UserToken;
-//import com.casestudy.service.AppUserService;
-//import com.casestudy.service.JwtService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
+package com.casestudy.controllers;
+
+import com.casestudy.model.LoginUser;
+import com.casestudy.model.UserRole;
+import com.casestudy.model.dto.UserToken;
+import com.casestudy.service.AppUserService;
+import com.casestudy.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
